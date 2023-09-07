7ec4bcc9f98e9862d5883da1afdb5f09c619cac7
Theo Santos <theo.santos@octo.com>
2023-09-07 08:35:41 +0000

    TP4

--- dags/predict_new_data.py
+++ dags/predict_new_data.py
@@ -14,13 +14,19 @@ from formation_indus_ds_avancee.data_loading import get_data_from_csv
 
 @dag(default_args={'owner': 'airflow'}, schedule=timedelta(minutes=2),
      start_date=pendulum.today('UTC').add(hours=-1))
-def predict():
+def predict_with_new_data():
+
+    @task
+    def get_data_from_csv_task():
+        get_data_from_csv(train_data_path=TRAIN_DATA_PATH, data_folder=GENERATED_DATA_FOLDER)
+        new_path = GENERATED_DATA_FOLDER + "/latest.csv"
+        return new_path
 
 
     @task
-    def prepare_features_with_io_task():
+    def prepare_features_with_io_task(new_path: str):
         features_path = os.path.join(DATA_FOLDER, f'prepared_features_{datetime.now()}.parquet')
-        prepare_features_with_io(data_path=GENERATED_DATA_PATH,
+        prepare_features_with_io(data_path=new_path,
                                  features_path=features_path,
                                  training_mode=False)
         return features_path
@@ -32,8 +38,9 @@ def predict():
             model_path=MODEL_PATH, 
             predictions_folder=PREDICTIONS_FOLDER)
     
-    features_path = prepare_features_with_io_task()
+    new_path = get_data_from_csv_task()
+    features_path = prepare_features_with_io_task(new_path)
     predict_with_io_task(features_path=features_path)
 
 
-predict_dag = predict()
+predict_with_new_data_dag = predict_with_new_data()
